5a6a34d33c82faa1d7e3885c0a5824d67701fa4d
shinigami <kometmontana22@gmail.com>
2022-10-21 23:05:05 +0300

    final commit

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
 
+
 gulp.task('server', function() {
 
     browserSync({
@@ -27,7 +28,8 @@ gulp.task('styles', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-})
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+});
+
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
